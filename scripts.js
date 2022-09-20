c4c6d1a3a8e2be3c791cab4c3b73fc860b19611c
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-09-20 18:56:14 -0500

    added new API to search tv shows

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
+// Testing api search
+let searchInput = "batman"
 
-async function getMovies() {
-    const  url = "https://fantasy-seasoned-leopard.glitch.me/movies"
+// Call to Tv Maze API to get search results 
+async function getTvMaze() {
+    const  url = `https://api.tvmaze.com/search/shows?q=${searchInput}`
     try {
         const response = await fetch(url);
         const data = await response.json();
@@ -9,4 +12,17 @@ async function getMovies() {
         console.log(error);
     }
 }
-getMovies();
+getTvMaze();
+
+// Call to glitch API to get favorite shows
+async function getFavorites() {
+    const  glitchUrl = "https://fantasy-seasoned-leopard.glitch.me/shows"
+    try {
+        const response = await fetch(glitchUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+getFavorites();
